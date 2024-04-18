31a4c4086d6dbec991fd04c9f05eb07ecdaac2a4
Karl Bartel <karl@karl.berlin>
2024-04-18 11:25:56 +0200

    Bump version 1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ spellfix1 = Extension("spellfix1", sources=["spellfix.c"])
 
 setup(
     name="sqlite-spellfix",
-    version="1.0",
+    version="1.1",
     description="Loadable spellfix1 extension for sqlite",
     py_modules=["sqlite_spellfix"],
     ext_modules=[spellfix1],
